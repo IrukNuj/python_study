7f610097e1ef1e498bed76a769a4864a4f03b17f
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-11-30 11:53:17 +0900

    jsonの整形

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from get_keywords import get_keywords
 from get_links import get_links
 
@@ -22,3 +23,7 @@ for dept, labs_url in zip(href_dict.keys(), href_dict.values()):
     labs_dict['labs'] = labs_value_list
     json_body.append(labs_dict)
     print(json_body)
+
+filename = 'labs.json'
+with open(filename, mode='w', encoding='utf-8') as f:
+    json.dump(json_body, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ':　'))
