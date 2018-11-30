173efc4eb76c276d229c6e701c51acd51a333417
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-11-30 10:12:06 +0900

    json化終了

--- mini_project2.py
+++ mini_project2.py
@@ -6,42 +6,35 @@ proxy = 'http://wwwproxy.kanazawa-it.ac.jp:8080'
 
 
 def get_keywords(url):
-    time.sleep(0.5)
+    time.sleep(0.1)
     print(url)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
     keywords_csv = soup.find('meta', attrs={'name': 'keywords'}).get('content')
     return keywords_csv.split(',')
 
-
-def get_dept_and_lab(url):
-    time.sleep(0.5)
+def get_links(url):
+    href_dict = {}
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
-    dept = soup.select_one('.subjectName p').text
-    lab = soup.select_one('.laboName .title').text
-    return dept, lab
+    links = soup.find_all(class_='subjectItem')
+
+    for l in links:
+        keywords_list = []
+        labs_list = []
+        [keywords_list.append(lhref.get('href')) for lhref in l.select('.laboList a')]
+        [labs_list.append(lhref.text) for lhref in l.select('.laboList a')]
+        href_dict[l.p.text] = keywords_list
+    return href_dict, labs_list
 
 
+
+URL_BASE = 'https://kitnet.jp/laboratories/'
+INDEX_URL = 'https://kitnet.jp/laboratories/index.html'
 render_dict = {}
-render_list = []
-keywords_list = []
+json_body = []
 
-url_base = 'https://kitnet.jp/laboratories/'
-
-r = requests.get('https://kitnet.jp/laboratories/index.html')
-soup = BeautifulSoup(r.content, 'lxml')
-
-links = soup.find_all(class_='subjectItem')
-
-
-href_dict = {}
-for l in links:
-    keywords_list = []
-    labs_list = []
-    [keywords_list.append(lhref.get('href')) for lhref in l.select('.laboList a')]
-    [labs_list.append(lhref.text) for lhref in l.select('.laboList a')]
-    href_dict[l.p.text] = keywords_list
+href_dict, labs_list = get_links(INDEX_URL)
 
 
 keywords_dict = {}
@@ -50,17 +43,19 @@ for dept, labs_url in zip(href_dict.keys(), href_dict.values()):
     labs_value_list = []
     for lab_url, lab in zip(labs_url, labs_list):
         labs_value_dict = {}
-        url = url_base + lab_url
+        url = URL_BASE + lab_url
         labs_value_dict['keywords'] = get_keywords(url)
         labs_value_dict['lab'] = lab
         labs_value_list.append(labs_value_dict)
-    labs_dict['dept'] = lab
+    labs_dict['dept'] = dept
     labs_dict['labs'] = labs_value_list
-    print(labs_dict)
+    json_body.append(labs_dict)
+    print(json_body)
 
 
 
 
+# イカゴミ
 
 render_dict['labs'] = []
 
