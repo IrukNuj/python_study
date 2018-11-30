c91799f76a28f1dc61dd766146856650d7c23583
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-11-30 12:52:58 +0900

    searchメソッドの修正

--- search.py
+++ search.py
@@ -1,15 +1,16 @@
 import json
 
 filename = 'labs.json'
-test_params = 'ビッグデータ'
+test_params = 'データマイニング'
 
 with open(filename, mode='r', encoding='utf-8') as f:
     data = json.load(f)
 
+result_list = []
+
 for d in data:
-    print(d)
     for dict in d['labs']:
         if test_params in dict['keywords']:
-            print(dict['lab'])
-            print('!?!?!?!??!????????????!!')
-    print('------------------------------------------------------------')
+            result_list.append(tuple([d['dept'], dict['lab']]))
+
+print(result_list)
