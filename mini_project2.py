6bfe040b70aa0ed1c761b176c85d6c20a4d09ceb
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-11-30 09:52:35 +0900

    indexから全情報取得

--- mini_project2.py
+++ mini_project2.py
@@ -7,6 +7,7 @@ proxy = 'http://wwwproxy.kanazawa-it.ac.jp:8080'
 
 def get_keywords(url):
     time.sleep(0.5)
+    print(url)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
     keywords_csv = soup.find('meta', attrs={'name': 'keywords'}).get('content')
@@ -25,14 +26,42 @@ def get_dept_and_lab(url):
 render_dict = {}
 render_list = []
 keywords_list = []
+dict_hoge = {}
+url_base = 'https://kitnet.jp/laboratories/'
 
 r = requests.get('https://kitnet.jp/laboratories/index.html')
 soup = BeautifulSoup(r.content, 'lxml')
 
 links = soup.find_all(class_='subjectItem')
 
+
+
 for l in links:
-    print(l)
+    keywords_list = []
+    labs_list = []
+    [keywords_list.append(lhref.get('href')) for lhref in l.select('.laboList a')]
+    [labs_list.append(lhref.text) for lhref in l.select('.laboList a')]
+    dict_hoge[l.p.text] = keywords_list
+    print(dict_hoge)
+
+keywords_dict = {}
+for hoge, huga in zip(dict_hoge.values(), dict_hoge.keys()):
+    labs_dict = {}
+    keywords_list = []
+    for i, hanya in zip(hoge, labs_list):
+        print('idayo-----', i)
+        url = url_base + i
+        keywords_dict['keywords'] = get_keywords(url)
+        print(keywords_dict)
+        keywords_list.append(keywords_dict)
+    labs_dict['dept'] = huga
+    labs_dict['labs'] = keywords_list
+    print(labs_dict)
+
+
+
+
+
 
 render_dict['labs'] = []
 
